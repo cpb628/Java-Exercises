f606f283bb6c86a2b9fc1dc835d4c2cf4895ecd1
Jun-Jin <candiberdin@c1-link.com>
2022-10-13 01:12:46 +0800

    exer3

---
+++ src/exer003_basic_math_compute.java
@@ -0,0 +1,19 @@
+
+public class exer003_basic_math_compute {
+	public static void main(String args[]) {
+		int num1 = 6, num2 = 2, result1, result2, result3, result4, result5;
+		
+		result1 = num1 + num2;
+		result2 = num1 * num2;
+		result3 = num1 / num2;
+		result4 = num1 - num2;
+		result5 = num1 % num2;
+		
+		System.out.println("Addition result is " + result1);
+		System.out.println("Multiplication result is " + result2);
+		System.out.println("Division result is " + result3);
+		System.out.println("Subtraction result is " + result4);
+		System.out.println("Modulus result is " + result5);
+		
+	}
+}
